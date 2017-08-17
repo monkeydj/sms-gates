b2b4b92b1989ebf37d784edf164b3121dba7617d
monkeydj <monkeydjiotaro@gmail.com>
2017-08-17 15:53:37 +0700

    try to get, but time doest not

--- pricings/twilio-pricing.js
+++ pricings/twilio-pricing.js
@@ -1,5 +1,11 @@
-var path = require("path");
-
 const Twilio_SID = "ACaefc6c3e9e99193c4bf05ed0bdf4fd3f";
 const API_Token = "e50f3af6cded7c2198bc8b0d23386aca";
-const URL = "https://pricing.twilio.com/v1";
+
+var twicli = require("twilio")(Twilio_SID, API_Token);
+
+console.log(twicli.pricing.messaging.countries("VN"));
+
+function callback(err, resp) {
+    console.log(resp);
+    if (err) throw err;
+}
